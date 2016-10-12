94a00acc1a2b5304cff219fdf9d1bc3cabe0ef84
arpanrau <arpan.rau@gmail.com>
2016-10-12 01:18:03 -0400

    Finished Mini Project 2

--- recursive_art.py
+++ recursive_art.py
@@ -1,41 +1,51 @@
-""" TODO: Put your header comment here """
-
+""" Generates random computational art and sets user's background image to said art """
 import random
+import math
 from PIL import Image
-
+import os
 
 def build_random_function(min_depth, max_depth):
     """ Builds a random function of depth at least min_depth and depth
-        at most max_depth (see assignment writeup for definition of depth
-        in this context)
-
+        at most max_depth 
         min_depth: the minimum depth of the random function
         max_depth: the maximum depth of the random function
         returns: the randomly generated function represented as a nested list
-                 (see assignment writeup for details on the representation of
-                 these functions)
     """
-    # TODO: implement this
-    pass
-
-
-def evaluate_random_function(f, x, y):
-    """ Evaluate the random function f with inputs x,y
-        Representation of the function f is defined in the assignment writeup
-
-        f: the function to evaluate
-        x: the value of x to be used to evaluate the function
-        y: the value of y to be used to evaluate the function
-        returns: the function value
-
-        >>> evaluate_random_function(["x"],-0.5, 0.75)
-        -0.5
-        >>> evaluate_random_function(["y"],0.1,0.02)
-        0.02
-    """
-    # TODO: implement this
-    pass
-
+    #List of possible 'building block' functions
+    prod = lambda a,b : a*b
+    avg = lambda a,b : .5*(a+b)
+    cos_pi = lambda a : math.cos(math.pi*a)
+    sin_pi = lambda a : math.sin(math.pi*a)
+    variableperiod = random.randrange(0,10)
+    sin_variableperiod = lambda a : math.cos(math.pi*a*variableperiod)
+    cos_variableperiod = lambda a : math.sin(math.pi*a*variableperiod)
+    x = lambda a,b : a
+    y = lambda a,b : b
+    sqrtdiff = lambda a : math.sqrt(1-a**2)
+    absolute = lambda a : abs(a)
+    #list of building block functions that take 1 argument
+    function_list1 = [cos_pi,sin_pi,sqrtdiff,absolute,cos_variableperiod,sin_variableperiod]
+    #list of building block functions that take 2 arguments
+    function_list2 = [prod,avg,x,y]
+    #list of all fucntions
+    function_list  = [prod,avg,cos_pi,sin_pi,x,y,sqrtdiff,absolute,cos_variableperiod,sin_variableperiod]
+    def generate(depth):
+        "recursively generates random function of specified depth."
+        if depth <= 0:
+            return random.choice([x,y])
+        else:
+            #pick a function from avaliable functions
+            funct = random.choice(function_list)
+            #generate inner function recursively and wrap
+            if funct in function_list1:
+                innerfunc = generate(depth - 1)
+                return lambda a,b : funct(innerfunc(a,b))
+            if funct in function_list2:
+                innerfunc = generate(depth - 1)
+                innerfunc2 = generate(depth - 1)
+                return lambda a,b : funct(innerfunc(a,b),innerfunc2(a,b))
+          
+    return generate(random.randrange(min_depth, max_depth))
 
 def remap_interval(val,
                    input_interval_start,
@@ -64,9 +74,13 @@ def remap_interval(val,
         >>> remap_interval(5, 4, 6, 1, 2)
         1.5
     """
-    # TODO: implement this
-    pass
-
+    #figure out scaling factor based on range sizes, find appropriate place in output range for input value
+    input_range = input_interval_end - input_interval_start
+    output_range = output_interval_end - output_interval_start
+    scaling_factor = float(output_range) / float(input_range)
+    return output_interval_start + (val - input_interval_start) * scaling_factor
+    
+  
 
 def color_map(val):
     """ Maps input value between -1 and 1 to an integer 0-255, suitable for
@@ -84,29 +98,8 @@ def color_map(val):
         >>> color_map(0.5)
         191
     """
-    # NOTE: This relies on remap_interval, which you must provide
     color_code = remap_interval(val, -1, 1, 0, 255)
     return int(color_code)
-
-
-def test_image(filename, x_size=350, y_size=350):
-    """ Generate test image with random pixels and save as an image file.
-
-        filename: string filename for image (should be .png)
-        x_size, y_size: optional args to set image dimensions (default: 350)
-    """
-    # Create image and loop over all pixels
-    im = Image.new("RGB", (x_size, y_size))
-    pixels = im.load()
-    for i in range(x_size):
-        for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            pixels[i, j] = (random.randint(0, 255),  # Red channel
-                            random.randint(0, 255),  # Green channel
-                            random.randint(0, 255))  # Blue channel
-
-    im.save(filename)
 
 
 def generate_art(filename, x_size=350, y_size=350):
@@ -116,9 +109,9 @@ def generate_art(filename, x_size=350, y_size=350):
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for red, green, and blue channels - where the magic happens!
-    red_function = ["x"]
-    green_function = ["y"]
-    blue_function = ["x"]
+    red_function = build_random_function(1,9)
+    green_function = build_random_function(1,9)
+    blue_function = build_random_function(1,9)
 
     # Create image and loop over all pixels
     im = Image.new("RGB", (x_size, y_size))
@@ -128,9 +121,9 @@ def generate_art(filename, x_size=350, y_size=350):
             x = remap_interval(i, 0, x_size, -1, 1)
             y = remap_interval(j, 0, y_size, -1, 1)
             pixels[i, j] = (
-                    color_map(evaluate_random_function(red_function, x, y)),
-                    color_map(evaluate_random_function(green_function, x, y)),
-                    color_map(evaluate_random_function(blue_function, x, y))
+                    color_map(red_function(x, y)),
+                    color_map(green_function(x, y)),
+                    color_map(blue_function(x, y)),
                     )
 
     im.save(filename)
@@ -140,11 +133,13 @@ if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-    # Create some computational art!
-    # TODO: Un-comment the generate_art function call after you
-    #       implement remap_interval and evaluate_random_function
-    # generate_art("myart.png")
+#generate art and place in pictures folder
 
-    # Test that PIL is installed correctly
-    # TODO: Comment or remove this function call after testing PIL install
-    test_image("noise.png")
+name = "/home/arpan/Pictures/myart0.png"
+generate_art(name,1920,1080)
+
+#set to system background
+os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/arpan/Pictures/myart0.png")
+
+
+
